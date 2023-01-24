78ee57816848a2f0df8d10ab4a41624130fc6891
hugo <hugo0703@wanadoo.fr>
2023-01-24 14:10:07 +0100

    environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+const express = require('express') //
+const dotenv = require('dotenv').config() // In order to have environnement variables
+const port = process.env.PORT || 5000
+
+const app = express() // initializes express
+
+app.listen(port, () => console.log(`Server running on ${port}`))
