d619841f65153e71fd2616e78fa8247f10045f1a
hugo <hugo0703@wanadoo.fr>
2023-01-24 17:47:46 +0100

    Accepting body data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,12 @@
 const getGoals = (req, res) => {
     res.status(200).json({message : 'get goals'})
 }
+
 const setGoals = (req, res) => {
+    console.log(req.body)
     res.status(200).json({message : 'Set goals'})
 }
+
 const updateGoal = (req, res) => {
     res.status(200).json({message: `goal ${req.params.id} modified`})
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals } = require('../controllers/goalController')
-const {postGoals, setGoals, updateGoal, deleteGoal} = require('../controllers/goalController')
+const { getGoals, setGoals, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-/*router.get('/', (req, res) => {
-    res.status(200).json({message: 'Get goals'})})*/ 
-    // What's above is replaced with : 
-    router.get('/', getGoals)
 
-router.post('/', setGoals)
+router.route('/').get(getGoals).post(setGoals)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
-router.put('/:id', updateGoal) 
 
-router.delete('/:id', deleteGoal)
 
 module.exports = router
