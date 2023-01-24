39d8aa52a3696b842cac9f1789604c27a52bb1a5
hugo <hugo0703@wanadoo.fr>
2023-01-24 14:51:04 +0100

    Route files

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 5000
 
 const app = express() // initializes express
 
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({message: 'Get goals'}) 
+})
+
 app.listen(port, () => console.log(`Server running on ${port}`))
