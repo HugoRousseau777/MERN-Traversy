acaf380866cbbf3cb29ad1bf288fa75486cb8f0a
hugo <hugo0703@wanadoo.fr>
2023-01-24 16:50:47 +0100

    all routes & POSTMAN works

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.status(200).json({message: 'Get goals'}) 
-})
+    res.status(200).json({message: 'Get goals'})})
+router.post('/', (req, res) => {
+    res.status(200).json({message: 'Set goals'})})
+router.put('/:id', (req, res) => {
+    res.status(200).json({message: `goal ${req.params.id} modified`})}) 
+router.delete('/:id', (req, res) =>{
+    res.status(200).json({message: `goal ${req.params.id} deleted`})})
 
 module.exports = router
