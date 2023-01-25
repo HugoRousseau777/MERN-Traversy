845b1e7a66dd3545c221ec8143719247c5203af5
hugo <hugo0703@wanadoo.fr>
2023-01-25 16:07:45 +0100

    DB created and connected

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express') //
 const dotenv = require('dotenv').config() // In order to have environnement variables
 const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express() // initializes express
 
