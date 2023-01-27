2e50a9e1726aba225a1732b02e692989ea5957e6
hugo <hugo0703@wanadoo.fr>
2023-01-27 13:49:20 +0100

    Video2Start

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -12,15 +12,39 @@ const setGoals = asyncHandler (async (req, res) => {
         res.status(400)
         throw new Error('Please add a text fieldgrgsgrz')
     }
-    res.status(200).json({message : 'Set goals'})
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 
 const updateGoal = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `goal ${req.params.id} modified`})
+
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+
+    res.status(200).json(updatedGoal)
 })
 
 const deleteGoal = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `goal ${req.params.id} deleted`})
+    const goal = await Goal.findById(req.params.id)
+    if (!goal){
+        res.status(400)
+        throw new Error('Goal non-existant')
+    }
+
+    await goal.remove()
+
+    res.status(200).json( { id:req.params.id})
 })
 
 module.exports = {
