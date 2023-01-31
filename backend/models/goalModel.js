3d2052e5f79eac13499c2418753f62d88a618acf
hugo <hugo0703@wanadoo.fr>
2023-01-31 16:43:34 +0100

    fin vidéo 2

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 const goalSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, // The type will be the id created with each creation of user
+        required: true,
+        ref: 'User' // Name of the required schema
+    },
     text: {
         type: String,
         required: [true, 'please add text']
